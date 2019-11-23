d1175d4ee91a058f09cb18a2ca79cd8cf2b9d1c2
ue72698 <alexander.striffeler@sbb.ch>
2019-11-23 11:54:10 +0100

    rename application to gamecenter-client

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# VolleycenterClient
+# GamecenterClient
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'volleycenter-client'`, () => {
+  it(`should have as title 'gamecenter-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('volleycenter-client');
+    expect(app.title).toEqual('gamecenter-client');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to volleycenter-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to gamecenter-client!');
   });
 });
