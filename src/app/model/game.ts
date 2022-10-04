27ff52e7734c0261dde80d697d59abbf1cca1a03
Alexander Striffeler <alexander.striffeler@avega.ch>
2022-10-04 17:34:08 +0200

    adopt to new json structure

--- src/app/model/game.ts
+++ src/app/model/game.ts
@@ -11,5 +11,5 @@ export interface Game {
   league: League;
   group: { groupId: number, caption: string };
   hall: Hall;
-  setResults: { 1, 2, 3, 4, 5: SetResult };
+  setResults: SetResult[];
 }
--- src/app/model/set-result.ts
+++ src/app/model/set-result.ts
@@ -1,4 +1,4 @@
 export interface SetResult {
   home: number;
-  away;
+  away: number;
 }
